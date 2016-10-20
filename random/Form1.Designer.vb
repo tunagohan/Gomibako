df30e7d9ab20a237e4f5aae9bd7ce90bc526516e
tunagohan <kazuxtuna@gmail.com>
2016-10-20 22:46:34 +0900

    complete
    release

---
+++ random/Form1.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(203, 81)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(63, 44)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "実行"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(82, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(147, 19)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 15)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 12)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "作業フォルダ："
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 61)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 12)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "生成内容設定"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(25, 84)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 12)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "サイズ："
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(71, 81)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(56, 19)
+        Me.TextBox3.TabIndex = 8
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Byte", "KB", "MB", "GB"})
+        Me.ComboBox1.Location = New System.Drawing.Point(133, 81)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(56, 20)
+        Me.ComboBox1.TabIndex = 9
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(71, 106)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(56, 19)
+        Me.TextBox4.TabIndex = 10
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 109)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(59, 12)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "生成個数："
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(133, 109)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(29, 12)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "（個）"
+        '
+        'Label9
+        '
+        Me.Label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label9.Location = New System.Drawing.Point(0, 45)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(284, 1)
+        Me.Label9.TabIndex = 14
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(235, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(31, 19)
+        Me.Button2.TabIndex = 15
+        Me.Button2.Text = "..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(8, 157)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(258, 14)
+        Me.ProgressBar1.TabIndex = 16
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(71, 135)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(132, 16)
+        Me.CheckBox1.TabIndex = 17
+        Me.CheckBox1.Text = "生成と同時に削除する"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 176)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "ゴミ箱埋める君"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents CheckBox1 As CheckBox
+End Class
---
+++ random/Form1.vb
@@ -0,0 +1,64 @@
+﻿Imports System
+Imports System.IO
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'コントロールを初期化する
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Maximum = TextBox4.Text
+        ProgressBar1.Value = 0
+        Dim DirBox1 As String = TextBox1.Text
+
+        '入力チェックを行う
+        If Not System.IO.Directory.Exists(TextBox1.Text) Then Exit Sub
+        If Not IsNumeric(TextBox3.Text) Then Exit Sub
+        If Not IsNumeric(TextBox4.Text) Then Exit Sub
+        If ComboBox1.SelectedIndex = -1 Then Exit Sub
+        '生成ファイルサイズを取得する
+        Dim sz As Integer = Val(TextBox3.Text) * 1024 ^ ComboBox1.SelectedIndex
+        '生成個数を取得する
+        Dim cnt As Integer = Val(TextBox4.Text)
+
+        '生成個数分くりかえす
+        For i As Integer = 1 To cnt
+            '一時ファイルを作成し、フルパス名を取得する
+            Dim tmpName As String = System.IO.Path.GetTempFileName()
+            '生成した一時ファイルのファイルサイズを変更する
+            Dim f As New System.IO.FileStream(tmpName, IO.FileMode.Open)
+            f.SetLength(sz)
+            f.Flush()
+            f.Close()
+            '生成した一時ファイルを作業フォルダに移動する
+            System.IO.File.Move(tmpName, System.IO.Path.Combine(TextBox1.Text, System.IO.Path.GetFileName(tmpName)))
+        Next
+
+        If CheckBox1.Checked = True Then
+            For Each tempFile As String In System.IO.Directory.GetFiles(DirBox1)
+                System.IO.File.Delete(tempFile)
+                ProgressBar1.Value = cnt
+            Next
+        Else
+
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim dlg As New FolderBrowserDialog
+        'FolderBrowserDialogを用いて作業フォルダを設定する
+        dlg.SelectedPath = TextBox1.Text
+        If dlg.ShowDialog(Me) = Windows.Forms.DialogResult.OK Then
+            TextBox1.Text = dlg.SelectedPath + "\"
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim stCurrentDir As String = System.IO.Directory.GetCurrentDirectory()
+        TextBox1.Text = stCurrentDir + "\" + "Dust" + "\"
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
+End Class
